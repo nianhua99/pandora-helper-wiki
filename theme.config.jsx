60e37f588e9a113fba7c041e43caeb0177d3b0a1
nianhua99 <1139168548@qq.com>
2024-09-21 18:33:38 +0800

    init

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,18 @@
 export default {
     logo: <span>Pandora Helper</span>,
     project: {
-        link: 'https://github.com/shuding/nextra'
+        link: 'https://github.com/nianhua99/PandoraHelper'
+    },
+    banner: {
+        text: (
+            <a href={'https://github.com/nianhua99/PandoraHelper'} target={'_blank'}>
+                如果帮到你了，为我点个Star⭐️吧！ Read More →
+            </a>
+        )
+    },
+    useNextSeoProps() {
+        return {
+            titleTemplate: '%s - Pandora Helper',
+        }
     }
-    // ... other theme options
 }
