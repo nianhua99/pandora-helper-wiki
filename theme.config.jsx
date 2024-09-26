c929e8b3d172b9c60d197c24301f56e4c2a2034b
nianhua99 <1139168548@qq.com>
2024-09-26 23:05:44 +0800

    init

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,5 +1,5 @@
 export default {
-    logo: <span>Pandora Helper</span>,
+    logo: <h1 className={"nx-text-3xl nx-font-bold"}>Pandora Helper</h1>,
     project: {
         link: 'https://github.com/nianhua99/PandoraHelper'
     },
@@ -10,6 +10,29 @@ export default {
             </a>
         )
     },
+    footer: {
+        text: (
+            <span>
+                <a href="https://github.com/nianhua99/PandoraHelper" target="_blank">
+                  Pandora Helper
+                </a>
+            </span>
+        )
+    },
+    feedback: {
+       content: null
+    },
+    editLink: {
+        component: null,
+    },
+    head: (
+        <>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta name="description" content="Pandora Helper" />
+            <meta name="keywords" content="Pandora Helper" />
+            <link rel="icon" href="/oai-icon.ico" />
+        </>
+    ),
     useNextSeoProps() {
         return {
             titleTemplate: '%s - Pandora Helper',
